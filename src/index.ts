dd99b39b7a8bd061ef8882280f688d8e7c2d0cd5
Igor De Azevedo Xavier Damieri De Morães <pet0160427@aluno.firjansenaisesi.com.br>
2026-04-20 21:29:35 -0300

    typeorm

--- src/index.ts
+++ src/index.ts
@@ -3,11 +3,13 @@ import express, { Application } from "express";
 import { AppDataSource } from "./data-source";
 import { userRoutes } from "./routes/userRoutes";
 import { postRoutes } from "./routes/postRoutes";
+import { errorMiddleWare } from "./middlewares/erroMiddleware";
 
 const app: Application = express();
 app.use(express.json());
 app.use("/api/posts", postRoutes);
 app.use("/api/users", userRoutes);
+app.use(errorMiddleWare);
 
 AppDataSource.initialize()
   .then(() => {
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -4,12 +4,12 @@ import { UserController } from "../controller/userController";
 const router = Router();
 const userControler = new UserController();
 
-router.post("/", (req, res) => userControler.create(req, res));
-router.get("/", (req, res) => userControler.list(req, res));
-router.get("/active", (req, res) => userControler.listActive(req, res));
-router.get("/:id", (req, res) => userControler.lsitById(req, res));
-router.delete("/:id", (req, res) => userControler.delete(req, res));
-router.patch("/:id", (req, res) => userControler.update(req, res));
+router.post("/", userControler.create);
+router.get("/", userControler.list);
+router.get("/active", userControler.listActive);
+router.get("/:id", userControler.lsitById);
+router.delete("/:id", userControler.delete);
+router.patch("/:id", userControler.update);
 router.patch("/:id/toggle", (req, res) => userControler.toggleActive(req, res));
 
 export const userRoutes = router;
